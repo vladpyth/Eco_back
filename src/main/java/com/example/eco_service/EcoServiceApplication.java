100b876b1cc2e6d518c1efaf6b2f527b7d4a8b78
Semenchykov Vladislav <v.semenchykov@gmail.com>
2026-03-11 15:30:36 +0300

    first commit

---
+++ src/main/java/com/example/eco_service/EcoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.eco_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoServiceApplication.class, args);
+    }
+
+}
